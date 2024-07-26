475e83d2c5ffa2233bf7de9f32e278a95b14d80a
JayaniAththanagoda <165479877+JayaniAththanagoda@users.noreply.github.com>
2024-07-26 15:58:25 +0530

    change logo

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./navbar.css";
-import logo from "../../Assets/logo.png";
+import logo from "../../Assets/newlog.png";
 
 import { Link } from "react-scroll";
 const Navbar = () => {
